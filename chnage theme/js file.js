8251050a9ec957c7bd48c8ad30ff2add98e0c5da
Mohammad Kamal <mohammadkamalsarwari@gmail.com>
2024-06-11 22:44:14 +0430

    Delete chnage theme directory

--- chnage theme/js file.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#button").click(function(){
-        $("body").toggleClass("change-background")
-        $("span").text($("span").text() === "Light Mode" ? "Dark Mode": "Light Mode")
-    });
-    $("#button").click(function(){
-        $(this).find("i").toggleClass("fa-moon-o fa-sun-o")
-    });
-});
